9257fcf83f603f769f7b070acab3b71a333ee1f1
LUXK18 <lucasgabrwork@gmail.com>
2026-09-22 23:37:51 -0300

    feat: adiciona imagem do produto ao resultado

--- server.mjs
+++ server.mjs
@@ -13,6 +13,7 @@ const mimeTypes = {
   ".js": "text/javascript; charset=utf-8",
   ".json": "application/json; charset=utf-8",
   ".mjs": "text/javascript; charset=utf-8",
+  ".png": "image/png",
   ".svg": "image/svg+xml; charset=utf-8"
 };
 
